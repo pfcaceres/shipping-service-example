6bad1c95237cc026b5bfc0b4355dd48ab1f7c9c2
pfcaceres <111323556+pfcaceres@users.noreply.github.com>
2022-12-13 20:57:30 -0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  profile    = "default"
+  region     = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "my-tf-test-bucket-miguel-19287319837892137"
+  acl    = "private"
+tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "my-tf-test-bucket-miguel-tfstate"
+    key    = "default-infrastructure.tfstate"
+    region = "us-east-1"
+  }
+}
