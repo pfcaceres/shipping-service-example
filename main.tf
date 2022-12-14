34d0f857decce11b846aba57728be847260e1f2e
pfcaceres <111323556+pfcaceres@users.noreply.github.com>
2022-12-13 23:20:17 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-resource "aws_vpc" "micro_vpc" {
-  cidr_block = "50.0.0.0/16"
+#resource "aws_vpc" "micro_vpc" {
+#  cidr_block = "50.0.0.0/16"
  
-}
+#}
 
 terraform {
   backend "s3" {
